d2270f35d6dd9e8c2823413cdf620a3ddf13f889
“tayyab212” <tthunjra@gmail.com>
2023-05-03 19:53:05 +0200

    Nesting classes and sealed

---
+++ ObjectOrientatedProgramming/NestingClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientatedProgramming
+{
+    //Class Inside class is nesting class
+    // only meaning to that class then nesting
+    //For legical grouping the classes
+    //connot access instace of outer classes directly but can pass
+    //can put any access modifier in nesting class
+   public class CustomerNestingClass
+    {
+        public int TotalBill { get; set; }
+        private class CustomerEligible
+        {
+            public bool IsEligible(CustomerNestingClass c)
+            {
+                if(c.TotalBill > 100)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ ObjectOrientatedProgramming/Sealed.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientatedProgramming
+{
+    // cannot be inherited any further
+    //can create an instace of class
+    public class A
+    {
+
+    }
+    public sealed class B : A
+    {
+
+    }
+
+    // below code will give compile time error 
+    //public class C : B
+    //{
+
+    //}
+
+}
